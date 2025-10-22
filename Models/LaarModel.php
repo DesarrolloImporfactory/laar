31aa59b15c67ec8956ef501536612c6c4aafde25
Jeimy Jara <desarrollo1@imporfactoryusa.com>
2025-10-22 16:34:24 -0500

    debug 4

--- Models/LaarModel.php
+++ Models/LaarModel.php
@@ -16,6 +16,7 @@ class LaarModel extends Query
     private $anotherServer;
     public function __construct()
     {
+        parent::__construct();
         $this->anotherServer = AnotherServer::getInstance();
         $config = [
             "host" => env('DB_ANOTHER_HOST', 'localhost'),
